939f9cca975701a2e41fc08077d811afa6ba1260
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2024-11-27 12:50:05 +0530

    "Split S3 bucket ACL into separate resource"

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl    = var.acl
   tags = var.tags
+}
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.acl
 }
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,3 +2,6 @@ output "bucket_arn" {
   description = "ARN of the S3 bucket"
   value       = aws_s3_bucket.bucket.arn
 }
+
+
+
