5cbea5c003595ae4607006feb84ea411d35820e1
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2024-12-16 13:03:07 +0530

    update s3 module

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,4 +1,23 @@
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   tags = var.tags
+}
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+    mfa_delete = false
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.kms_key_arn
+    }
+  }
 }
